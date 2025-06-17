f904147be5f76db08642056833438fa8dd6a9c03
Sneha Yadav <164773937+SNH078@users.noreply.github.com>
2025-06-17 14:30:27 +0530

    css issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,24 @@ import Favorites from "./pages/favorites";
 import Details from "./pages/details";
 
 function App() {
-  return (
-    <div>
-      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/recipe-item/:id" element={<Details />} />
-        </Routes>
-        <Footer/>
-      </div>
+ return (
+  <div className="min-h-screen flex flex-col bg-white text-gray-600 text-lg ">
+    <div className="p-4">
+         <Navbar />
     </div>
-  );
+ 
+    <div className="flex-grow p-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/recipe-item/:id" element={<Details />} />
+      </Routes>
+    </div>
+
+    <Footer />
+  </div>
+);
+
 }
 
 export default App;
