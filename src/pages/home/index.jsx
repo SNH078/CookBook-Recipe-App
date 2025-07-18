7cf767bac28089ff30afb3ccdffeea228989ff4e
Sneha Yadav <164773937+SNH078@users.noreply.github.com>
2025-07-18 22:47:57 +0530

    alert message ...

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -20,7 +20,7 @@ export default function Home() {
 
   <button
     className="bg-pink-300 text-grey px-4 py-2 rounded-lg hover:bg-pink-500 transition"
-   onClick={() => alert("Not sure what to try? start with searching for 'Paneer' or 'Pasta' 🍲")}
+   onClick={() => alert("Not sure what to try? start with searching for 'Biryani' or 'Pasta' 🍲")}
 
   >
     Show Me Something Good
